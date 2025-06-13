59c044cc827359624c17dd159514659a63ca15d8
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-06-13 14:55:14 -0300

    Implements chart title and message when user doesn't has public repo

--- lib/modules/user-details/presentation/widgets/repos_bar_chart.dart
+++ lib/modules/user-details/presentation/widgets/repos_bar_chart.dart
@@ -22,76 +22,94 @@ class ReposBarChart extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 1.4,
-            child: BarChart(
-              BarChartData(
-                barGroups: List.generate(sorted.length, (index) {
-                  final data = sorted[index];
-                  return BarChartGroupData(
-                    x: index,
-                    barRods: [
-                      BarChartRodData(
-                        toY: data.commitCount.toDouble(),
-                        width: 20,
-                        color: barColors[index % barColors.length],
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                    ],
-                  );
-                }),
-                titlesData: FlTitlesData(
-                  leftTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                  bottomTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                  rightTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                  topTitles:
-                      AxisTitles(sideTitles: SideTitles(showTitles: false)),
-                ),
-                borderData: FlBorderData(show: false),
-                gridData: FlGridData(show: false),
+      child: Visibility(
+        visible: sorted.isNotEmpty,
+        replacement: Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Nenhum repositório encontrado',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Repositórios atualizados recentemente',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(sorted.length, (index) {
-              final data = sorted[index];
-              final color = barColors[index % barColors.length];
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 16,
-                      height: 16,
-                      margin: const EdgeInsets.only(right: 8),
-                      decoration: BoxDecoration(
-                        color: color,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${data.repoName} | ${data.commitCount} commits',
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ],
+            const SizedBox(height: 15),
+            AspectRatio(
+              aspectRatio: 1.4,
+              child: BarChart(
+                BarChartData(
+                  barGroups: List.generate(sorted.length, (index) {
+                    final data = sorted[index];
+                    return BarChartGroupData(
+                      x: index,
+                      barRods: [
+                        BarChartRodData(
+                          toY: data.commitCount.toDouble(),
+                          width: 20,
+                          color: barColors[index % barColors.length],
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ],
+                    );
+                  }),
+                  titlesData: FlTitlesData(
+                    leftTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    bottomTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    rightTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    topTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  ),
+                  borderData: FlBorderData(show: false),
+                  gridData: FlGridData(show: false),
                 ),
-              );
-            }),
-          ),
-        ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(sorted.length, (index) {
+                final data = sorted[index];
+                final color = barColors[index % barColors.length];
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 16,
+                        height: 16,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                          color: color,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '${data.repoName} | ${data.commitCount} commits',
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
