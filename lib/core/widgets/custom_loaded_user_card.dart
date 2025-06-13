be8b95418d95081dae2a7898fc9dd13848ebc88f
FigueredoMatheus <matheusfigueredob@gmail.com>
2025-06-13 13:09:32 -0300

    Implements navigation to user details page when loaded user card is tapped

--- lib/core/widgets/custom_loaded_user_card.dart
+++ lib/core/widgets/custom_loaded_user_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_user_finder/core/navigation/app_routes_names.dart';
 import 'package:github_user_finder/modules/search-user/domain/entities/user.dart';
 
 class CustomLoadedUserCard extends StatelessWidget {
@@ -8,31 +9,40 @@ class CustomLoadedUserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 1,
-      borderRadius: BorderRadius.circular(10),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: NetworkImage(user.avatarUrl),
-              maxRadius: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: Text(
-                user.login,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          AppRoutesNames.USER_DETAILS,
+          arguments: user,
+        );
+      },
+      child: Material(
+        elevation: 1,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.avatarUrl),
+                maxRadius: 30,
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  user.login,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
